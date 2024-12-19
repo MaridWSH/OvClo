82905d6a4ab93be3cd56b1a1f3880a4f57782d1a
Abdelrehman Fouad <97409061+MaridWSH@users.noreply.github.com>
2024-12-19 12:49:10 +0200

    GameFunctions.java

--- GameFunctions.java
+++ GameFunctions.java
@@ -4,22 +4,10 @@ import java.util.Random;
 
 public class GameFunctions {
     static final String[] COLORS = {"R", "B", "G", "Y", "P", "C", "W", "O"}; 
-
-//    static String generatecode() {
-//        List<String> chosenColors = new ArrayList<>();
-//        Random rand = new Random();
-//        while (chosenColors.size() < 4) {
-//            String color = COLORS[rand.nextInt(COLORS.length)];
-//            if (!chosenColors.contains(color)) {
-//                chosenColors.add(color);
-//            }
-//        }
-//        return String.join("", chosenColors); 
-//    }
     static String generatecode() {
     String code = "";
     while (code.length() < 4) {
-        int rand = (int)(Math.random() * 8); // Random number between 0-7
+        int rand = (int)(Math.random() * 8);
         String color = COLORS[rand];
         if (!code.contains(color)) {
             code += color;
@@ -28,7 +16,7 @@ public class GameFunctions {
     return code;
 }
     
-    static boolean validateinput(String text) { // negative logic 
+    static boolean validateinput(String text) {  
         if (text.length() != 4) return true;
 
         for (char c : text.toCharArray()) {
@@ -43,21 +31,4 @@ public class GameFunctions {
         }
         return false;
     }
-    
-//    static boolean isValid(String text) {
-//    if (text.length() != 4) return false;
-//
-//    for (char c : text.toCharArray()) {
-//        boolean isValid = false;
-//        for (String validColor : COLORS) {
-//            if (String.valueOf(c).equalsIgnoreCase(validColor)) {
-//                isValid = true;
-//                break;
-//            }
-//        }
-//        if (!isValid) return false;
-//    }
-//    return true;
-//}
-
 }
